ffa3d98802a9469413233376dcb6c9e764576ede
Flirty82 <flirting.singles2015@gmail.com>
2025-07-13 23:38:59 -0500

    integrated services and moved

---
+++ client/src/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9AHimDF9Jo_w_B3meAUK9lsq-yU08ivE",
+    authDomain: "f-singles-24.firebase.com",
+    projectId: "f-singles-24",
+    storageBucket: "f-singles-24.firebase-storage.app",
+    messagingSenderId: "961233473100",
+    appId: "1:961233473100:web:db446c19f603c0fd7aaee9",
+    measurementId: "G-9GYFF0B1ET"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
