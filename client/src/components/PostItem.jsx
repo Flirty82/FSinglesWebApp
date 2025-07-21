9e0015bcbeac792a8f5b93e25085dc4a25949ff6
Flirty82 <flirting.singles2015@gmail.com>
2025-07-21 00:03:49 -0500

    deleted more duplicate files and cleaned up project

--- client/src/components/PostItem.jsx
+++
@@ -1,8 +0,0 @@
-export default function PostItem({ post }) {
-    return (
-        <div classname="post">
-            <p><strong>{post.username}</strong></p>
-            <p>{post.content}</p>
-            <p><small>{new Date(post.createdAt).toLocaleString()}</small></p>
-        </div>
-)}
--- server/routes/chatRoomRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatRoomController = require('../controllers/chatRoomController');
-const auth = require('../middleware/authMiddleware');
-
-router.get('/:roomName', auth, chatRoomController.getRoomMessages);
-router.post('/:roomName', auth, chatRoomController.addRoomMessage);
-
-module.exports = router;
